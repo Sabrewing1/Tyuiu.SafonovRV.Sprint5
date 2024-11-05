fff011b0673952541d8b57c4d95c1978b5d8a139
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-11-05 19:47:56 +0500

    Task7

--- Tyuiu.SafonovRV.Sprint5.Task7.V27/Program.cs
+++ Tyuiu.SafonovRV.Sprint5.Task7.V27/Program.cs
@@ -6,32 +6,40 @@ namespace Tyuiu.SafonovRV.Sprint5.Task7.V27
     {
         static void Main(string[] args)
         {
-            
-            {
-                DataService ds = new DataService();
-                Console.WriteLine("***************************************************************************");
-                Console.WriteLine("* Исходные данные:                                                        *");
-                Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
 
+            Console.Title = "Спринт #5 | Выполнила: Сафонов Р.В | АСОиУБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание: 7                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнила: Сафонов Руслан Васильевич                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, в котором есть набор символьных данных.Удалить все      *");
+            Console.WriteLine("*  идущие подряд больше одного..Полученный результат сохранить в файл     *");
+            Console.WriteLine("* OutPutDataFileTask7V27.txt.                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
 
-                string path = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
-                string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V27.txt";
+            string path = $@"C:\Users\rusla\source\repos\Tyuiu.SafonovRV.Sprint5\Tyuiu.SafonovRV.Sprint5.Task7.V27\bin\Debug\net8.0\OutPutDataFileTask7V27";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7.txt";
 
+            Console.WriteLine($"Данные находятся в файле: {path}");
 
-                Console.WriteLine("Данные находяться в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
 
-                Console.WriteLine("***************************************************************************");
-                Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-                Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Находится в файле: ");
 
-                Console.WriteLine("Находится в файле : ");
-                pathSaveFile = ds.LoadDataAndSave(path);
-                Console.WriteLine(pathSaveFile);
-                Console.ReadKey();
-
-            }
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
         }
     }
 }
-    
+
 
