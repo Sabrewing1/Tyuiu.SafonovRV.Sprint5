3a3c3bf61dbcb3158463f5dde79fab7b0eee4162
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-11-09 13:14:41 +0500

    Task4

--- Tyuiu.SafonovRV.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint5.Task4.V5.Lib/DataService.cs
@@ -8,12 +8,11 @@ namespace Tyuiu.SafonovRV.Sprint5.Task4.V5.Lib
             public double LoadFromDataFile(string path)
             {
             double x = 1.0;
-            double y = 0;
             string strX = File.ReadAllText(path);
             strX = strX.Replace(".", ",");
             var X = Convert.ToDouble(strX);
 
-            double res =  y = 4.26 * x / Math.Sin(x);
+            double res =    (4.26 * x) / Math.Sin(x);
             res = Math.Round(res, 3);
                 return res;
             }
